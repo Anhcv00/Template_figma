fa3f5558979245f3a85f712295912e03d486305b
Lemon <57029749+Anhcv00@users.noreply.github.com>
2024-10-09 13:21:58 +0700

    update header

---
+++ code/js/index.js
@@ -0,0 +1,9 @@
+// Scroll headers
+window.addEventListener('scroll', function() {
+    const header = document.getElementById('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
