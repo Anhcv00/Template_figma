c8c48b4fc7b6a0ccbe24003eb98ad454db5f4acb
Lemon <57029749+Anhcv00@users.noreply.github.com>
2024-10-14 15:28:04 +0700

    responsive impact

--- js/index.js
+++ js/index.js
@@ -1,9 +1,29 @@
-// Scroll headers
+// Cuộn headers
 window.addEventListener('scroll', function() {
     const header = document.getElementById('header');
     if (window.scrollY > 50) {
         header.classList.add('scrolled');
     } else {
         header.classList.remove('scrolled');
+    }
+});
+
+// Lấy phần tử nút toggle và menu
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+const toggleIcon = menuToggle.querySelector('i');
+
+// Thêm sự kiện click để mở/đóng menu
+
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+
+    if (menuToggle.classList.contains('active')) {
+        toggleIcon.classList.remove('fa-bars');
+        toggleIcon.classList.add('fa-caret-up');
+    } else {
+        toggleIcon.classList.remove('fa-caret-up');
+        toggleIcon.classList.add('fa-bars');
     }
 });
